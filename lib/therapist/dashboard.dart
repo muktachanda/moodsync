00933cbec56b8dcc2506d4765f2ed0049941a595
muktachanda <muktachanda03@gmail.com>
2024-03-02 23:12:44 +0530

    improved dashboard ui

--- lib/therapist/dashboard.dart
+++ lib/therapist/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 import 'dart:math';
 
 class Dashboard extends StatefulWidget {
@@ -12,24 +13,25 @@ class _DashboardState extends State<Dashboard> {
   List<Map<String, dynamic>> patients = [
     {
       'name': 'Alice',
-      'tags': ['Depressed', 'Anxiety'],
+      'tags': ['Depressed', 'At-risk'],
+      'sessions': 8,
+      'progress': 70,
     },
     {
       'name': 'Bob',
       'tags': ['Suicidal', 'At-risk'],
+      'sessions': 12,
+      'progress': 90,
     },
     {
       'name': 'Eve',
       'tags': ['Normal'],
+      'sessions': 5,
+      'progress': 40,
     },
   ];
 
   List<String> tags = ['Depressed', 'Anxiety', 'Suicidal', 'At-risk', 'Normal'];
-
-  List<String> notificationHistory = [
-    'Bob seems suicidal. Please check in with him.',
-    'Alice needs help.',
-  ];
 
   bool showNotifications = false;
   List<String> selectedTags = [];
@@ -44,81 +46,104 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Therapist Dashboard',
-          style: TextStyle(fontSize: 24.0),
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.dark(
+          primary: Colors.blue,
+          secondary: Colors.green,
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.notifications),
-            onPressed: () {
-              setState(() {
-                showNotifications = !showNotifications;
-              });
-            },
-          ),
-        ],
       ),
-      body: showNotifications
-          ? ListView.builder(
-        itemCount: notificationHistory.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(
-              notificationHistory[index],
-              style: TextStyle(fontSize: 20.0),
-            ),
-          );
-        },
-      )
-          : Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Text(
-              'Filter by Tags:',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Therapist Dashboard',
+            style: TextStyle(fontSize: 24.0),
           ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: tags.map((tag) {
-                return FilterChip(
-                  label: Text(
-                    tag,
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                  selected: selectedTags.contains(tag),
-                  onSelected: (bool selected) {
-                    setState(() {
-                      if (selected) {
-                        selectedTags.add(tag);
-                      } else {
-                        selectedTags.remove(tag);
-                      }
-                      filterPatients();
-                    });
-                  },
+          actions: [
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NotificationHistory()),
                 );
-              }).toList(),
+              },
             ),
-          ),
-          SizedBox(height: 20.0),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: DataTable(
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'Dashboard Overview',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildPieChart(
+                      dataMap: getTagsChartData(),
+                      title: 'Tags',
+                    ),
+                    _buildPieChart(
+                      dataMap: getSessionChartData(),
+                      title: 'Num Sessions',
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'Filter by Tags:',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: tags.map((tag) {
+                    return FilterChip(
+                      label: Text(
+                        tag,
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      selected: selectedTags.contains(tag),
+                      onSelected: (bool selected) {
+                        setState(() {
+                          if (selected) {
+                            selectedTags.add(tag);
+                          } else {
+                            selectedTags.remove(tag);
+                          }
+                          filterPatients();
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              DataTable(
                 columnSpacing: 40.0,
                 columns: [
                   DataColumn(label: Text('Patient', style: TextStyle(fontSize: 20.0))),
                   DataColumn(label: Text('Tags', style: TextStyle(fontSize: 20.0))),
+                  DataColumn(label: Text('Sessions', style: TextStyle(fontSize: 20.0))),
                   DataColumn(label: Text('Progress', style: TextStyle(fontSize: 20.0))),
                 ],
                 rows: filteredPatients.map((patient) {
@@ -154,11 +179,17 @@ class _DashboardState extends State<Dashboard> {
                       ),
                     ),
                     DataCell(
+                      Text(
+                        patient['sessions'].toString(),
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                    DataCell(
                       Container(
                         width: 150,
                         height: 30,
                         child: LinearProgressIndicator(
-                          value: 0.5, // Placeholder value for progress (50%)
+                          value: patient['progress'] / 100,
                           backgroundColor: Colors.grey[300],
                           valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
                         ),
@@ -167,9 +198,9 @@ class _DashboardState extends State<Dashboard> {
                   ]);
                 }).toList(),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
@@ -185,4 +216,109 @@ class _DashboardState extends State<Dashboard> {
       }).toList();
     });
   }
+
+  Widget _buildPieChart({required Map<String, double> dataMap, required String title}) {
+    return Column(
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10.0),
+        PieChart(
+          dataMap: dataMap,
+          animationDuration: Duration(milliseconds: 800),
+          chartLegendSpacing: 32.0,
+          chartRadius: MediaQuery.of(context).size.width / 5,
+          colorList: _generateRandomColors(dataMap.length, context),
+        ),
+      ],
+    );
+  }
+
+  Map<String, double> getTagsChartData() {
+    Map<String, double> dataMap = {};
+    patients.forEach((patient) {
+      (patient['tags'] as List<String>).forEach((tag) {
+        dataMap[tag] = (dataMap[tag] ?? 0) + 1;
+      });
+    });
+    return dataMap;
+  }
+
+  Map<String, double> getSessionChartData() {
+    Map<String, double> dataMap = {};
+    filteredPatients.forEach((patient) {
+      dataMap[patient['name']] = patient['sessions'].toDouble();
+    });
+    return dataMap;
+  }
+
+  List<Color> _generateRandomColors(int count, BuildContext context) {
+    List<Color> themeColors = [
+      Color(0xFF0D47A1), // Blue
+      Color(0xFF1565C0), // Blue
+      Color(0xFF1976D2), // Blue
+      Color(0xFF1E88E5), // Blue
+      Color(0xFF2196F3), // Blue
+      Color(0xFF42A5F5), // Blue
+      Color(0xFF64B5F6), // Blue
+      Color(0xFF90CAF9), // Blue Light
+      Color(0xFFBBDEFB), // Blue Lighter
+    ];
+
+    List<Color> colors = [];
+    Random random = Random();
+    for (int i = 0; i < count; i++) {
+      colors.add(themeColors[random.nextInt(themeColors.length)]);
+    }
+    return colors;
+  }
+
+}
+
+class NotificationHistory extends StatelessWidget {
+  final List<String> notificationHistory = [
+    'Bob seems suicidal. Please check in with him.',
+    'Alice needs help.',
+  ];
+
+  NotificationHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notification History'),
+      ),
+      body: Container(
+        color: Colors.grey[900], // Dark background color
+        child: ListView.builder(
+          itemCount: notificationHistory.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              color: Colors.grey[800], // Card background color
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  notificationHistory[index],
+                  style: TextStyle(
+                    color: Colors.white, // Text color
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(Dashboard());
 }
