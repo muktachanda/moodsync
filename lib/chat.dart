e531d9fe4232e9183d1198b23ffb720467ab1d8b
nandinimaroo <nandini.maroo@research.iiit.ac.in>
2024-03-02 20:23:24 +0530

    added chat

---
+++ lib/chat.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'therapist/dashboard.dart';
+import 'patient/patient_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Therapist and Patient',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/therapist_dashboard': (context) => const Dashboard(),
+        '/patient_screen': (context) => const PatientScreen(),
+      },
+    );
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose User Type'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/therapist_dashboard');
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 40.0),
+                ),
+                child: Text(
+                  'Therapist',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/patient_screen');
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 40.0),
+                ),
+                child: Text(
+                  'Patient',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
